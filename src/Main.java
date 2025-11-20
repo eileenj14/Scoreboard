5afb1fff56267e9a0b0fae31d434c9e342ab013b
Eileen Jiang <eileenj14@nycstudents.net>
2025-11-20 11:50:01 -0500

    Scoreboard in progress.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
         Scoreboard game = new Scoreboard("Red", "Blue");
         System.out.println(game.getScore());
@@ -17,9 +22,37 @@ public class Main
         game.recordPlay(0);
         game.recordPlay(4);
         game.recordPlay(0);
-        System.out.println(game.getScore());;
+        System.out.println(game.getScore());
         Scoreboard match = new Scoreboard("Lions", "Tigers");
         System.out.println(match.getScore());
         System.out.println(game.getScore());
+        System.out.println();
+        teams();
+    }
+
+    public static void teams() throws FileNotFoundException
+    {
+        File f = new File("Scoreboard.txt");
+        Scanner s = new Scanner(f);
+        String[] teams = new String[1];
+        teams[0] = s.next();
+        while(s.hasNext())
+        {
+            String team = s.next();
+            for(int i = 0; i < teams.length; i++)
+            {
+                if(team == teams[i]) break;
+                if((i + 1) == teams.length)
+                {
+                    int newSize = teams.length + 1;
+                    teams = Arrays.copyOf(teams, newSize);
+                    teams[teams.length - 1] = team;
+                }
+            }
+        }
+        for(int i = 0; i < teams.length; i++)
+        {
+            System.out.println(i + " " + teams[i]);
+        }
     }
 }
