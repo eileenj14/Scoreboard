6f71339b9fcf458070e169a1c3f8e774ec6b05d2
eileenj14 <eileenj14@nycstudents.net>
2025-11-20 23:24:51 -0500

    Scoreboard in progress.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.FileNotFoundException;
 import java.io.File;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main
@@ -27,32 +26,46 @@ public class Main
         System.out.println(match.getScore());
         System.out.println(game.getScore());
         System.out.println();
-        teams();
+        System.out.println(read());
     }
 
-    public static void teams() throws FileNotFoundException
+    public static String read() throws FileNotFoundException
     {
         File f = new File("Scoreboard.txt");
         Scanner s = new Scanner(f);
-        String[] teams = new String[1];
+        String[] teams = new String[numOfTeams()];
+        int[] teamWins = new int[numOfTeams()];
+        while(s.hasNextLine())
+        {
+            Scanner ss = new Scanner(s.nextLine());
+        }
+        for(int i = 0; i < numOfTeams(); i++)
+        {
+            System.out.println(teams[i] + " " + teamWins[i]);
+        }
+        return null;
+    }
+
+    public static int numOfTeams() throws FileNotFoundException
+    {
+        File f = new File("Scoreboard.txt");
+        Scanner s = new Scanner(f);
+        String[] teams = new String[25];
         teams[0] = s.next();
+        int numOfTeams = 1;
         while(s.hasNext())
         {
             String team = s.next();
-            for(int i = 0; i < teams.length; i++)
+            for(int i = 0; i < numOfTeams; i++)
             {
-                if(team == teams[i]) break;
-                if((i + 1) == teams.length)
+                if(team.equals(teams[i])) break;
+                if(i + 1 == numOfTeams)
                 {
-                    int newSize = teams.length + 1;
-                    teams = Arrays.copyOf(teams, newSize);
-                    teams[teams.length - 1] = team;
+                    teams[numOfTeams] = team;
+                    numOfTeams++;
                 }
             }
         }
-        for(int i = 0; i < teams.length; i++)
-        {
-            System.out.println(i + " " + teams[i]);
-        }
+        return numOfTeams;
     }
 }
